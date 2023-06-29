b531da8067bb0fa9fd6b31f51857cd8ccf9f2fc4
teppei1994667 <teppei.engineer.667@gmail.com>
2023-06-29 23:20:56 +0900

    password_digestの存在チェック、最小文字数のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,8 @@ class User < ApplicationRecord
 	# emailのバリデーション
 	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, 
 										uniqueness: true
+
+	validates :password, presence: true, length: {minimum: 6} 
+
 	has_secure_password
 end
