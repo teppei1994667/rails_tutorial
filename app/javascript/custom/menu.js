d5bfdee7d90ba97fbf3b5fc28e03d460cde02d88
teppei1994667 <teppei.engineer.667@gmail.com>
2023-07-02 23:54:55 +0900

    モバイル向けのメニューデザイン実装

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -2,6 +2,12 @@
 
 // トグルリスナーを追加してクリックをリッスンする
 document.addEventListener("turbo:load", () => {
+	const hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", (event) => {
+    event.preventDefault();
+    const menu = document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse");
+  });
   const account = document.querySelector("#account");
   account.addEventListener("click", (event) => {
     event.preventDefault();
