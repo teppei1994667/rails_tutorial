64f08cec73adf2b03cf6869310a81c9ffed1afe8
teppei1994667 <teppei.engineer.667@gmail.com>
2023-07-02 16:58:24 +0900

    ユーザー認証処理仮実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to user
+    else
+      render "new", status: :unprocessable_entity
+    end
+    
+  end
+
+  def destroy
+
+  end
 end
