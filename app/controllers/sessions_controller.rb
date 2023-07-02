1fcae26af9788821d51c4a455b8d75c6a40e8bba
teppei1994667 <teppei.engineer.667@gmail.com>
2023-07-02 17:23:29 +0900

    log_inヘルパーの実装とユーザー認証時の処理実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      reset session  # ログインの直前に必ずこれを書くこと(セッション固定攻撃への対策)
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,5 @@
 module SessionsHelpe
+	def log_in(user)
+		session[:user_id] = user.id
+	end
 end
