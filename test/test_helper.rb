1b87f84cca27ce5e6f7fca60538d6dacc361adb7
teppei1994667 <teppei.engineer.667@gmail.com>
2023-07-03 22:55:40 +0900

    ユーザー登録後自動ログインテスト実装

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,5 +12,15 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
-  # Add more helper methods to be used by all tests here...
+  class ActiveSupport::TestCase
+    # 指定のワーカー数でテストを並列実行する
+    parallelize(workers: :number_of_processors)
+    # test/fixtures/*.ymlのfixtureをすべてセットアップする
+    fixtures :all
+  
+    # テストユーザーがログイン中の場合にtrueを返す
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+  end
 end
