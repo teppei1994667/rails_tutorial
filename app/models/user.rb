3090ebda7bb253b32bed4a3a99b59f26a0a38c5a
teppei1994667 <teppei.engineer.667@gmail.com>
2023-07-05 23:04:26 +0900

    記憶トークンの作成と暗号化実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+	attr_accessor :remenber_token
+
 	# emailの一意性のバリデーションのためにメールアドレスを全て小文字に変換
 	before_save { self.email = email.downcase }
 	# nameのバリデーション
@@ -19,4 +22,15 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+	# ランダムなトークンを返す
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	 # 永続セッションのためにユーザーをデータベースに記憶する
+	def remenber
+		self.remenber_token = User.new_token
+		update_attribute(:remenber_digest, User.digest(remenber_token))
+	end
 end
