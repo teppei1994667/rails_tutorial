e110e83bd37bf0b559dc1045e0757242f3822ad3
teppei1994667 <teppei.engineer.667@gmail.com>
2023-07-09 19:07:29 +0900

    sessionヘルパーの各メソッドにコメント追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,20 +1,25 @@
 module SessionsHelper
-	def log_in(user)
-		session[:user_id] = user.id
-	end
 
-	def current_user
-		if session[:user_id]
-			@current_user ||= User.find_by(id: session[:user_id])
-		end
-	end
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-	def logged_in?
-		!current_user.nil?
-	end
+  # 現在ログインしているユーザーを返す（いる場合）
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 
-	def log_out
-		reset_session
-		@current_user = nil # 安全のため
-	end
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    reset_session
+    @current_user = nil
+  end
 end
