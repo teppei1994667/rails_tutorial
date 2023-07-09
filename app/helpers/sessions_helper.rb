f3ab493e39d001568120ab172a7c7102bbd0c52a
teppei1994667 <teppei.engineer.667@gmail.com>
2023-07-09 19:31:07 +0900

    session_helperにremember実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # 現在ログインしているユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
