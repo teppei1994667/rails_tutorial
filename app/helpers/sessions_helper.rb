3d3641f1a3d3dd7423ffdc3ac4f9ec2e7951370c
teppei1994667 <teppei.engineer.667@gmail.com>
2023-07-09 19:41:30 +0900

    current_userにcookiesの考慮を実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,10 +12,18 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 現在ログインしているユーザーを返す（いる場合）
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
+		#（ユーザーIDにユーザーIDのセッションを代入した結果）ユーザーIDのセッションが存在すれば
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
+		#（ユーザーIDにユーザーIDのcookiesを代入した結果）ユーザーIDのcookiesが存在すれば
+		elsif (user_id = cookies.encrypted[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remenber_token])
+				log_in user
+				@current_user = user
+			end
     end
   end
 
